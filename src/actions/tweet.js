c0d66be4893fb39d88bcf7b10e4e6e7e7482dbb5
rbc33 <ricardobenthem@gmail.com>
2025-02-18 20:36:44 +0100

    Revert "8.11 Like Tweet Action"
    This reverts commit 0ea218fb1eb99780812e7ca88af52e73262d92cd.

--- src/actions/tweet.js
+++ src/actions/tweet.js
@@ -1,7 +1,4 @@
-import { saveLikeToggle } from "../utils/helpers";
-
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
-export const TOGGLE_TWEET = "TOGGLE_TWEET";
 
 export function receiveTweets(tweets) {
 	return {
@@ -9,23 +6,3 @@ export function receiveTweets(tweets) {
 		tweets,
 	};
 }
-
-const toggleTweet = ({ id, authUser, hasLiked }) => {
-	return {
-		type: TOGGLE_TWEET,
-		id,
-		authUser,
-		hasLiked,
-	};
-};
-
-export const handleToggleTweet = (info) => {
-	return (dispatch) => {
-		dispatch(toggleTweet(info));
-		return saveLikeToggle(info).catch((e) => {
-			console.warn("Error in handleToggleTweet: ", e);
-			dispatch(toggleTweet(info));
-			alert("There was an error liking the tweet. Try again.");
-		});
-	};
-};
