8b201a0bc1575a8a508955d1821acd0f40e79b85
rbc33 <ricardobenthem@gmail.com>
2025-02-18 19:10:39 +0100

    8.10 Tweet UI 2

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -7,13 +7,9 @@ const AUTHED_ID = "tylermcginnis";
 
 export const handleInitialData = () => {
 	return (dispatch) => {
-		// Asegurarse de que authUser es null al inicio
-		dispatch(setAthedUser(null));
-
 		return getInitialData().then(({ users, tweets }) => {
 			dispatch(receiveTweets(tweets));
 			dispatch(receiveUsers(users));
-			// Establecer el usuario autenticado después de cargar los datos
 			dispatch(setAthedUser(AUTHED_ID));
 		});
 	};
